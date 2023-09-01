4c864a52d10f17aceaccd4141f8772cbe67431d6
BrianKawinzi <brian.mutisya@starthmore.edu>
2023-09-01 07:19:01 +0300

    sign in button

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medrx/components/my_button.dart';
 import 'package:medrx/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,6 +8,9 @@ class LoginPage extends StatelessWidget {
   // text editing controllers
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+
+  //sign user in method
+  void signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
@@ -48,12 +52,31 @@ class LoginPage extends StatelessWidget {
               MyTextField(
                 controller: passwordController,
                 hintText: 'Password',
-                obscureText: false,
+                obscureText: true,
               ),
               
+
+              const SizedBox(height: 10),
               // forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
               //sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
               
+              const SizedBox(height: 50),
               
               //or continue with
               
